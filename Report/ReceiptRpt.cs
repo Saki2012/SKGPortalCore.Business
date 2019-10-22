bd04864c8c43e8fe7a5c27d20c3fa52345e8a881
Saki2012 <32598251+Saki2012@users.noreply.github.com>
2019-10-22 15:45:47 +0800

    20191022

---
+++ Report/ReceiptRpt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using pdftron.PDF;
+using pdftron.SDF;
+using Convert = pdftron.PDF.Convert;
+
+namespace SKGPortalCore.Business.Report
+{
+    public class ReceiptRpt
+    {
+
+        private readonly Dictionary<string, string> Dic = new Dictionary<string, string>()
+        {
+            {"RealAccountName","" },
+            {"TradeYear","" },
+            {"TradeMonth","" },
+            {"PrintDateCHN","" },
+        };
+
+        public void ExportPdf()
+        {
+            using PDFDoc pdfdoc = new PDFDoc();
+            Convert.OfficeToPDF(pdfdoc, $"{ReportTemplate.TemplatePath}{ReportTemplate.ReceiptTemplate}.docx", null);
+            Page pg = pdfdoc.GetPage(1);
+            ContentReplacer replacer = new ContentReplacer();
+            SetData();
+            foreach (string key in Dic.Keys) replacer.AddString(key, Dic[key]);
+            replacer.Process(pg);
+            pdfdoc.Save($"{ReportTemplate.TemplateOutputPath}{ReportTemplate.ReceiptTemplate}{ReportTemplate.Resx}.pdf", SDFDoc.SaveOptions.e_linearized);
+        }
+
+        public void SetData() 
+        {
+            Dic["RealAccountName"] = string.Empty;
+        }
+    }
+}
---
+++ Report/ReportTemplate.cs
@@ -0,0 +1,26 @@
+﻿namespace SKGPortalCore.Business.Report
+{
+    public class ReportTemplate
+    {
+        /// <summary>
+        /// 模板位置
+        /// </summary>
+        public const string TemplatePath = @"D:/";
+        /// <summary>
+        /// 
+        /// </summary>
+        public const string TemplateOutputPath = @"D:/";
+        /// <summary>
+        /// 帳單模板
+        /// </summary>
+        public const string BillTemplate = "BillTemplate";
+        /// <summary>
+        /// 收據模板
+        /// </summary>
+        public const string ReceiptTemplate = "ReceiptTemplate";
+        /// <summary>
+        /// 
+        /// </summary>
+        public static string Resx { get { return ""; } }
+    }
+}
